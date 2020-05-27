c855f2135537d6f17f8fec1bd0089031aa0c539e
nousi <nousidayo@gmail.com>
2020-05-27 09:37:24 +0900

    indexページのみグラデーションに設定
    もっと見るを押してもらうために、最後の文字をグラーデーションで隠す

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -14,7 +14,7 @@
       <svg class="bd-placeholder-img card-img-top" width="100%" height="200" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
       <div class="card-body">
         <h5 class="card-title">{{$tweet->title}}</h5>
-        <p class="card-text">{{$tweet->text}}</p>
+        <p class="card-text readmore-content">{{$tweet->text}}</p>
 
         <a href="{{ route('tweets.show', $tweet->id) }}" class="btn btn-primary">もっと見る</a>
       </div>
