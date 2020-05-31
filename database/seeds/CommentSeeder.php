e5f68a90208ef340e4ab04c24815048e754a21a1
nousi <nousidayo@gmail.com>
2020-05-31 16:44:23 +0900

    commentのseederの記述
    factoryによって、生成される

--- database/seeds/CommentSeeder.php
+++ database/seeds/CommentSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Model\Comment;
 
 class CommentSeeder extends Seeder
 {
@@ -11,6 +12,6 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(Comment::class, 200)->create();
     }
 }
