7732b2e8f086790e7788efaa5a1d511a6b227c50
nousi <nousidayo@gmail.com>
2020-05-31 16:40:55 +0900

    commentのfactoryを記述

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -6,7 +6,11 @@ use App\Model\Comment;
 use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
+    $tweetIDs  = App\Model\Tweet::pluck('id')->all();
+    $userIDs  = App\User::pluck('id')->all();
     return [
-        //
+        'text' => $faker->realText(30),
+        'tweet_id' => $faker->randomElement($tweetIDs),
+        'user_id' => $faker->randomElement($userIDs),
     ];
 });
